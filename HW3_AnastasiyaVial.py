29cb836017b9a21c11f73bd918434a2e49bf637c
Anastasiya Vial <anastasiya.vial.osv@fedex.com>
2022-10-05 14:02:05 +0200

    updates for HW3

--- HW3_AnastasiyaVial.py
+++ HW3_AnastasiyaVial.py
@@ -41,23 +41,29 @@ print(count_whitespaces)
 # in order to take last word of each sentence i did following:
 #  divide all text for sentences using "."
 e = []
+
 for i in d.split('.'):
-    e.append(i.split())
-# print(e)
+    for j in i.split():
+        e.append(i.split()[-1])
+print(e)
 
-#save each sentence into variable
-e1=e[0]
-e2=e[1]
-e3=e[2]
-e4=e[3]
-e5=e[4]
-e6=e[5]
-e7=e[6]
-e8=e[7]
-
-#create variable and add all last words from each sentence
-last_word_of_each_sentence_at_the_end = d + " " +e1[-1] + " " +e2[-1] + " " +e3[-1]+ " " +e4[-1]+ " " +e5[-1]+ " " +e6[-1]+ " " +e7[-1]+ " " +e8[-1]
-print(last_word_of_each_sentence_at_the_end)
+# for i in d.split('.'):
+#     e.append(i.split())
+# # print(e)
+#
+# #save each sentence into variable
+# e1=e[0]
+# e2=e[1]
+# e3=e[2]
+# e4=e[3]
+# e5=e[4]
+# e6=e[5]
+# e7=e[6]
+# e8=e[7]
+#
+# #create variable and add all last words from each sentence
+# last_word_of_each_sentence_at_the_end = d + " " +e1[-1] + " " +e2[-1] + " " +e3[-1]+ " " +e4[-1]+ " " +e5[-1]+ " " +e6[-1]+ " " +e7[-1]+ " " +e8[-1]
+# print(last_word_of_each_sentence_at_the_end)
 
 
 # i tried to do in some other way
